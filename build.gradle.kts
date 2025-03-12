b867f240f66e977ca669d7012159128744864080
Israel Rios <israel.rios@serpro.gov.br>
2025-03-12 10:32:58 -0300

    Supporting PHP and Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,27 @@
+/*
+ * Copyright (c) 2025  Israel Rios
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 plugins {
     id("java")
     id("org.jetbrains.intellij.platform") version "2.1.0"
 }
 
 group = "com.rios"
-version = "1.0.1"
+version = "1.0.2"
 
 repositories {
     mavenCentral()
@@ -23,6 +40,8 @@ dependencies {
         bundledPlugin("com.intellij.properties")
         bundledPlugin("com.intellij.java")
         bundledPlugin("JavaScript")
+        bundledPlugin("org.jetbrains.kotlin")
+        plugin("com.jetbrains.php", "243.25659.59")
     }
 }
 
